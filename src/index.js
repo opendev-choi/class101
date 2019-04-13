f88a5ec527f022d4649ef5c9994301b6d0937429
JongHyeok Choi <opendev.choi@gmail.com>
2019-04-13 14:05:37 +0900

    index.js 조회 부분 추가
    index.js 조회 하는 기능 추가
    - User, Post, Comment 조회 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,38 +1,56 @@
 const express = require('express');
+const SQLite = require('./sqlite.js');
 
 const app = express();
 
+console.log(SQLite['user'])
+
 // - Post
-app.get('/post', function (req, res) {
+app.get('/post/:id', function (req, res) {
+    console.log(SQLite['post'].findAll(
+        {attributes: ['post_id', 'author', 'title', 'contents', 'date'],
+            where: {post_id: req.params.id}}).then(post => {
+        res.send(JSON.stringify(post, null, 4));
+    }));
 });
 
-app.post('/post', function (req, res) {
+app.post('/post/:id', function (req, res) {
 });
 
-app.put('/post', function (req, res) {
+app.put('/post/:id', function (req, res) {
 });
 
-app.delete('/post', function (req, res) {
+app.delete('/post/:id', function (req, res) {
 });
 
 // - comment
-app.get('/post', function (req, res) {
+app.get('/comment/:id', function (req, res) {
+    console.log(SQLite['comment'].findAll(
+        {attributes: ['comment_id', 'author', 'contents', 'date'],
+            where: {comment_id: req.params.id}}).then(comment => {
+        res.send(JSON.stringify(comment, null, 4));
+    }));
 });
 
-app.post('/post', function (req, res) {
+app.post('/comment/:id', function (req, res) {
 });
 
-app.put('/post', function (req, res) {
+app.put('/comment/:id', function (req, res) {
 });
 
-app.delete('/post', function (req, res) {
+app.delete('/comment/:id', function (req, res) {
 });
 
 // - user
-app.get('/post', function (req, res) {
+app.get('/user/:id', function (req, res) {
+    console.log(SQLite['user'].findAll(
+        {attributes: ['user_id', 'name', 'regdate'],
+            where: {user_id: req.params.id}}).then(users => {
+        res.send(JSON.stringify(users, null, 4));
+    }));
 });
 
-app.post('/post', function (req, res) {
+app.post('/user/:id', function (req, res) {
 });
 
 app.listen(3000, function () {
