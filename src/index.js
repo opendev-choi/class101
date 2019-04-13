5e0c726f36919216a5c33821b5abe3173b773fe2
JongHyeok Choi <opendev.choi@gmail.com>
2019-04-13 13:09:47 +0900

    index.js 작성
    기본 구조의 index.js 작성
    - REST API 구조 작성

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+// - Post
+app.get('/post', function (req, res) {
+});
+
+app.post('/post', function (req, res) {
+});
+
+app.put('/post', function (req, res) {
+});
+
+app.delete('/post', function (req, res) {
+});
+
+// - comment
+app.get('/post', function (req, res) {
+});
+
+app.post('/post', function (req, res) {
+});
+
+app.put('/post', function (req, res) {
+});
+
+app.delete('/post', function (req, res) {
+});
+
+// - user
+app.get('/post', function (req, res) {
+});
+
+app.post('/post', function (req, res) {
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
